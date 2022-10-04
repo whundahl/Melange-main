e531ab125ea0273a80a71aa1c8f20f7acb70075c
Will Laptop <whundahl145@gmail.com>
2022-10-03 21:10:53 -0400

    fix

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -15,8 +15,8 @@ const sumfetch = async (args: string[]): Promise<string> => {
 ▐▓ M E L A N G E  V E N T U R E S  ▐▓        CONTACT 
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓          -----------
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           
       ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            
         ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
             ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
